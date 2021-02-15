30a6744bc56fb70f92fdc76d62b3f8947af1fd2f
Christian de la Cruz <chrisdlc119@outlook.com>
2021-02-15 03:50:56 -0600

    wayne: Add me as maintainer

--- aosp_wayne.mk
+++ aosp_wayne.mk
@@ -27,6 +27,7 @@ $(call inherit-product, device/xiaomi/wayne/device.mk)
 # Inherit some common stuff from PixelExtended
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 PEX_BUILD_TYPE := OFFICIAL
+PEX_MAINTAINER := chdelacr
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_STOCK_ARCORE := true
